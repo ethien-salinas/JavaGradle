c60f4e0b190e9e5dc896039e97a7167f5137de74
Ethien Salinas <ethien.salinas@gmail.com>
2017-09-09 09:42:56 -0500

    ADD Cookie to servlet

--- src/main/java/org/certificatic/TestServlet.java
+++ src/main/java/org/certificatic/TestServlet.java
@@ -1,6 +1,8 @@
 package org.certificatic;
 
 import javax.servlet.RequestDispatcher;
+import javax.servlet.http.Cookie;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -26,8 +28,13 @@ public class TestServlet extends javax.servlet.http.HttpServlet {
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
         RequestDispatcher rd = request.getRequestDispatcher("view.jsp");
+        // use of session object
         request.getSession().setAttribute("sessionAttribute", "Ethien sessionAttribute");
         System.out.println(request.getSession().getAttribute("sessionAttribute"));
+        // use of cookie
+        Cookie cookie = new Cookie("javax.servlet.http.Cookie", "value inside javax.servlet.http.Cookie");
+        response.addCookie(cookie);
+        System.out.println(request.getSession().getId());
         rd.forward(request, response);
     }
 }
